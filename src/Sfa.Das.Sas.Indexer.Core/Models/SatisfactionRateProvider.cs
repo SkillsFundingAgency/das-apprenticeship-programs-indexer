cad9b962c8cee4bee0330750c884b50814e48199
sachin.patel <sachin.patel@valtech.co.uk>
2016-11-07 14:05:08 +0000

    FAT-154 - Created SatisfactionRateProvider model.

---
+++ src/Sfa.Das.Sas.Indexer.Core/Models/SatisfactionRateProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sfa.Das.Sas.Indexer.Core.Models
+{
+    public class SatisfactionRateProvider
+    {
+        public long Ukprn { get; set; }
+
+        public double? FinalScore { get; set; }
+
+        public int LearnerCount { get; set; }
+
+        public int ResponseCount { get; set; }
+    }
+}
