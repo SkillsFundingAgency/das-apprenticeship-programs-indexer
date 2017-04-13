54b67674fefad5aff150530f53a40a11708a2f86
Scott Cowan <info@scottcowan.org>
2017-04-13 12:08:36 +0100

    adding tests for the date checks

---
+++ src/Sfa.Das.Sas.Indexer.UnitTests/Core/Models/LarsStandardTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Sfa.Das.Sas.Indexer.Core.Apprenticeship.Models.Standard;
+
+namespace Sfa.Das.Sas.Indexer.UnitTests.Core.Models
+{
+    [TestFixture]
+    class LarsStandardTests
+    {
+        [Test]
+        public void ShouldBeValidIfItHasNoEndDate()
+        {
+            // Arrange
+            var sut = new LarsStandard
+            {
+                EffectiveFrom = DateTime.MinValue,
+                EffectiveTo = null
+            };
+
+            // Act
+            var result = sut.IsValidDate(DateTime.UtcNow);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void ShouldBeInvalidIfItHasNoStartDate()
+        {
+            // Arrange
+            var sut = new LarsStandard
+            {
+                EffectiveFrom = null,
+                EffectiveTo = null
+            };
+
+            // Act
+            var result = sut.IsValidDate(DateTime.UtcNow);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void ShouldBeInvalidIfTheStartDateIsInTheFuture()
+        {
+            // Arrange
+            var sut = new LarsStandard
+            {
+                EffectiveFrom = DateTime.MaxValue,
+                EffectiveTo = null
+            };
+
+            // Act
+            var result = sut.IsValidDate(DateTime.UtcNow);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void ShouldBeInvalidIfTheEndDateIsInThePast()
+        {
+            // Arrange
+            var sut = new LarsStandard
+            {
+                EffectiveFrom = DateTime.MaxValue,
+                EffectiveTo = DateTime.UtcNow.AddYears(-1)
+            };
+
+            // Act
+            var result = sut.IsValidDate(DateTime.UtcNow);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+    }
+}
