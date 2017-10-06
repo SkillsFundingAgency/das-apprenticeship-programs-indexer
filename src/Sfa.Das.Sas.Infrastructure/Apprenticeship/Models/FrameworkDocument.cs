cb7f79234e9846620afdf7455a74c326c1661b29
Josevi Agullo <josevi.agullo@gmail.com>
2017-10-06 11:09:09 +0100

    CI-415 Avoiding FrameworkID analysis

--- src/Sfa.Das.Sas.Infrastructure/Apprenticeship/Models/FrameworkDocument.cs
+++ src/Sfa.Das.Sas.Infrastructure/Apprenticeship/Models/FrameworkDocument.cs
@@ -9,6 +9,7 @@ namespace Sfa.Das.Sas.Indexer.Infrastructure.Apprenticeship.Models
 {
     public sealed class FrameworkDocument : ApprenticeshipDocument, IIndexEntry
     {
+        [String(Index = FieldIndexOption.NotAnalyzed)]
         public string FrameworkId { get; set; }
 
         public bool Published { get; set; }
