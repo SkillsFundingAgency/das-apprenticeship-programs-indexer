9d90a586cff75129e48146be4b667ea2820ded4d
Josevi Agullo <josevi.agullo@gmail.com>
2017-10-11 10:21:25 +0100

    CI-71 Adding mapping when creating the index

--- src/Sfa.Das.Sas.Infrastructure/Provider/ElasticSearch/ElasticsearchProviderIndexMaintainer.cs
+++ src/Sfa.Das.Sas.Infrastructure/Provider/ElasticSearch/ElasticsearchProviderIndexMaintainer.cs
@@ -35,7 +35,12 @@
                 i => i
                     .Settings(settings => settings
                         .NumberOfShards(_elasticsearchConfiguration.ProviderIndexShards())
-                        .NumberOfReplicas(_elasticsearchConfiguration.ProviderIndexReplicas())));
+                        .NumberOfReplicas(_elasticsearchConfiguration.ProviderIndexReplicas()))
+                    .Mappings(ms => ms
+                        .Map<ProviderDocument>(m => m.AutoMap())
+                        .Map<ProviderApiDocument>(m => m.AutoMap())
+                        .Map<FrameworkProvider>(m => m.AutoMap())
+                        .Map<StandardProvider>(m => m.AutoMap())));
         }
 
         public void IndexProviders(string indexName, ICollection<Provider> entries)
