82bdf6ea7577e8c5d30e942ff5da62acb8ac2e79
froberg <christofer.froberg@gmail.com>
2017-12-18 11:26:05 +0000

    Fix CR issues, added a test and made it async

--- src/Sfa.Das.Sas.Indexer.ApplicationServices/Provider/Models/HeiProvidersResult.cs
+++ src/Sfa.Das.Sas.Indexer.ApplicationServices/Provider/Models/HeiProvidersResult.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 
-namespace Sfa.Das.Sas.Indexer.Core.Provider.Models
+namespace Sfa.Das.Sas.Indexer.ApplicationServices.Provider.Models
 {
     public class HeiProvidersResult
     {
---
+++ src/Sfa.Das.Sas.Infrastructure/Provider/Handlers/HeiProviderVstsHandler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Dfe.Edubase2.SoapApi.Client;
+using Dfe.Edubase2.SoapApi.Client.EdubaseService;
+using MediatR;
+
+using SFA.DAS.NLog.Logger;
+using Sfa.Das.Sas.Indexer.ApplicationServices.Provider.Models;
+
+namespace Sfa.Das.Sas.Indexer.Infrastructure.Provider.Handlers
+{
+    public sealed class HeiProviderVstsHandler : IAsyncRequestHandler<HeiProvidersRequest, HeiProvidersResult>
+    {
+        private readonly ILog _logger;
+
+        private readonly IEstablishmentClient _client;
+
+        public HeiProviderVstsHandler(
+            ILog logger,
+            IEstablishmentClient client)
+        {
+            _logger = logger;
+            _client = client;
+        }
+
+        public async Task<HeiProvidersResult> Handle(HeiProvidersRequest message)
+        {
+            var apiProviders = (await GetHeiUkprns())
+                .ToList();
+
+            _logger.Info($"Found ${apiProviders.Count} provider with establishment type of Higher Education");
+
+            return new HeiProvidersResult { Providers = apiProviders };
+        }
+
+        private async Task<IEnumerable<string>> GetHeiUkprns()
+        {
+            var establishments = await _client.FindEstablishmentsAsync(
+                new EstablishmentFilter
+                    {
+                        Page = 0,
+                        TypeOfEstablishment = EstablishmentType.HigherEducationInstitutions,
+                        Fields = new StringList { "UKPRN" }
+                    });
+
+            return establishments.Select(m => m.UKPRN).Where(m => m != null).Distinct().Select(m => m.ToString());
+        }
+    }
+}
