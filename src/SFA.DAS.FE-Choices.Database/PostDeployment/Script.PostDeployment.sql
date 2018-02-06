4dcaec7cb55e7380f67148670c49e31e5858a1f2
froberg <christofer.froberg@gmail.com>
2018-02-06 13:24:44 +0000

    added post deloy to pop hybridyear field

---
+++ src/SFA.DAS.FE-Choices.Database/PostDeployment/Script.PostDeployment.sql
@@ -0,0 +1,17 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+UPDATE [dbo].[EmployerSatisfaction] SET HybridYear = CONVERT(varchar(4), (Year-1)) + '/' + CONVERT(varchar(4), (Year))
+WHERE HybridYear = ''
+
+UPDATE [dbo].[LearnerSatisfaction] SET HybridYear = CONVERT(varchar(4), (Year-1)) + '/' + CONVERT(varchar(4), (Year))
+WHERE HybridYear = ''
