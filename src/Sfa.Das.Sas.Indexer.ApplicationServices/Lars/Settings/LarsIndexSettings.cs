87bed6aea0b2b8da86e640e58cb809c98befe608
Noble <e.noble@live.co.uk>
2018-03-02 11:53:30 +0000

    Local config and implement cloudconfigurationmanager

--- src/Sfa.Das.Sas.Indexer.ApplicationServices/Lars/Settings/LarsIndexSettings.cs
+++ src/Sfa.Das.Sas.Indexer.ApplicationServices/Lars/Settings/LarsIndexSettings.cs
@@ -1,13 +1,14 @@
 ﻿namespace Sfa.Das.Sas.Indexer.ApplicationServices.Apprenticeship.Settings
 {
     using System.Configuration;
+    using Microsoft.Azure;
     using Sfa.Das.Sas.Indexer.ApplicationServices.Apprenticeship.Services;
     using Sfa.Das.Sas.Indexer.ApplicationServices.Lars.Services;
     using Sfa.Das.Sas.Indexer.ApplicationServices.Shared.Settings;
 
     public class LarsIndexSettings : IIndexSettings<IMaintainLarsIndex>
     {
-        public string IndexesAlias => ConfigurationManager.AppSettings["LarsIndexAlias"];
+        public string IndexesAlias => CloudConfigurationManager.GetSetting("LarsIndexAlias");
 
         public string PauseTime => ConfigurationManager.AppSettings["PauseTime"];
 
