ef35157f17a075cc448d5f6d23a63566da555de0
Phil Davies <github@phildavies.co.uk>
2019-07-29 15:06:07 +0100

    update SSA Code scripts wip

---
+++ src/SFA.DAS.FE-Choices.Database/Manual/CON-643-UpdateAchievementRates/UpdateNationalSsaCodes.sql
@@ -0,0 +1,68 @@
+update [AchievementRatesNational] set SSA1 = 1, SSA2 = 1.1 where [Sector Subject Area Tier 2] = 'Medicine and Dentistry'
+update [AchievementRatesNational] set SSA1 = 1, SSA2 = 1.2 where [Sector Subject Area Tier 2] = 'Nursing and Subjects and Vocations Allied to Medicine'
+update [AchievementRatesNational] set SSA1 = 1, SSA2 = 1.3 where [Sector Subject Area Tier 2] = 'Health and Social Care'
+update [AchievementRatesNational] set SSA1 = 1, SSA2 = 1.4 where [Sector Subject Area Tier 2] = 'Public Services'
+update [AchievementRatesNational] set SSA1 = 1, SSA2 = 1.5 where [Sector Subject Area Tier 2] = 'Child Development and Well Being'
+
+update [AchievementRatesNational] set SSA1 = 2, SSA2 = 2.1 where [Sector Subject Area Tier 2] = 'Science'
+-- no National `Mathematics and Statistics` achievement rates? perhaps no-one apprentices in maths!? checked with...
+--   select distinct [Sector Subject Area Tier 2]
+--     FROM [SFA.DAS.FE-Choices.Database].[dbo].[AchievementRatesNational]
+-- where [Sector Subject Area Tier 1] = 'Science and Mathematics'
+update [AchievementRatesNational] set SSA1 = 2, SSA2 = 2.2 where [Sector Subject Area Tier 2] = 'Mathematics and Statistics'
+
+update [AchievementRatesNational] set SSA1 = 3, SSA2 = 3.1 where [Sector Subject Area Tier 2] = 'Agriculture'
+update [AchievementRatesNational] set SSA1 = 3, SSA2 = 3.2 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 3, SSA2 = 3.3 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 3, SSA2 = 3.4 where [Sector Subject Area Tier 2] = ''
+
+update [AchievementRatesNational] set SSA1 = 4, SSA2 = 4.1 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 4, SSA2 = 4.2 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 4, SSA2 = 4.3 where [Sector Subject Area Tier 2] = ''
+
+update [AchievementRatesNational] set SSA1 = 5, SSA2 = 5.1 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 5, SSA2 = 5.2 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 5, SSA2 = 5.3 where [Sector Subject Area Tier 2] = ''
+
+update [AchievementRatesNational] set SSA1 = 6, SSA2 = 6.1 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 6, SSA2 = 6.2 where [Sector Subject Area Tier 2] = ''
+
+update [AchievementRatesNational] set SSA1 = 7, SSA2 = 7.1 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 7, SSA2 = 7.2 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 7, SSA2 = 7.3 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 7, SSA2 = 7.4 where [Sector Subject Area Tier 2] = ''
+
+update [AchievementRatesNational] set SSA1 = 8, SSA2 = 8.1 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 8, SSA2 = 8.2 where [Sector Subject Area Tier 2] = ''
+
+update [AchievementRatesNational] set SSA1 = 9, SSA2 = 9.1 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 9, SSA2 = 9.2 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 9, SSA2 = 9.3 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 9, SSA2 = 9.4 where [Sector Subject Area Tier 2] = ''
+
+update [AchievementRatesNational] set SSA1 = 10, SSA2 = 10.1 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 10, SSA2 = 10.2 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 10, SSA2 = 10.3 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 10, SSA2 = 10.4 where [Sector Subject Area Tier 2] = ''
+
+update [AchievementRatesNational] set SSA1 = 11, SSA2 = 11.1 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 11, SSA2 = 11.2 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 11, SSA2 = 11.3 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 11, SSA2 = 11.4 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 11, SSA2 = 11.4 where [Sector Subject Area Tier 2] = ''
+
+update [AchievementRatesNational] set SSA1 = 12, SSA2 = 12.1 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 12, SSA2 = 12.2 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 12, SSA2 = 12.3 where [Sector Subject Area Tier 2] = ''
+
+update [AchievementRatesNational] set SSA1 = 13, SSA2 = 13.1 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 13, SSA2 = 13.2 where [Sector Subject Area Tier 2] = ''
+
+update [AchievementRatesNational] set SSA1 = 14, SSA2 = 14.1 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 14, SSA2 = 14.2 where [Sector Subject Area Tier 2] = ''
+
+update [AchievementRatesNational] set SSA1 = 15, SSA2 = 15.1 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 15, SSA2 = 15.2 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 15, SSA2 = 15.3 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 15, SSA2 = 15.4 where [Sector Subject Area Tier 2] = ''
+update [AchievementRatesNational] set SSA1 = 15, SSA2 = 15.4 where [Sector Subject Area Tier 2] = ''
