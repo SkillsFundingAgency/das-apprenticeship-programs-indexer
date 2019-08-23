2f930ec4bbd8a3e3629648480f8b36b13eb9c350
Lee Wadhams <lee.wadhams@valtech.co.uk>
2019-08-23 07:57:43 +0100

    Make address items non-analyzed keywords

--- src/Sfa.Das.Sas.Infrastructure/Provider/Models/ElasticSearch/Address.cs
+++ src/Sfa.Das.Sas.Infrastructure/Provider/Models/ElasticSearch/Address.cs
@@ -4,8 +4,10 @@ namespace Sfa.Das.Sas.Indexer.Infrastructure.Provider.Models.ElasticSearch
 {
     public sealed class Address
     {
+        [Keyword]
         public string Address1 { get; set; }
 
+        [Keyword]
         public string Address2 { get; set; }
 
         public string Town { get; set; }
